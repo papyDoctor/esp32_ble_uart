64682ab9be8669651d0334daead29df2d2737d10
Olivier Monnom <olivier.monnom@gmail.com>
2025-06-15 17:04:13 +0200

    refacto

--- src/task_led.rs
+++ src/task_led.rs
@@ -1,15 +1,15 @@
 use core::marker::Sized;
 use embassy_executor;
-use esp_hal::gpio::{Level, Output};
-use esp_println::println;
 
 #[embassy_executor::task]
 pub async fn task_led(gpio8: esp_hal::peripherals::GPIO8<'static>) {
-    println!("task_led started");
-    // Configure GPIO8 as push-pull output for LED
-    let mut led = Output::new(gpio8, Level::Low, esp_hal::gpio::OutputConfig::default());
+    let mut led = esp_hal::gpio::Output::new(
+        gpio8,
+        esp_hal::gpio::Level::Low,
+        esp_hal::gpio::OutputConfig::default(),
+    );
     loop {
         led.toggle();
-        embassy_time::Timer::after_millis(500).await;
+        embassy_time::Timer::after_millis(50000).await;
     }
 }
